7d190738115adb16269f9b29ecb805de2f398488
tigerforest0573 <49645154+tigerforest0573@users.noreply.github.com>
2025-06-04 00:18:54 +0800

    Add files via upload

---
+++ function/check.php
@@ -0,0 +1,19 @@
+<?php
+    function isAllChinese($str){
+        if(strpos($str,'·')){
+            //将·去掉，看看剩下的是不是都是中文
+            $str=str_replace("·",'',$str);
+            if(preg_match('/^[\x7f-\xff]+$/', $str)){
+                return true;//全是中文
+            }else{
+                return false;//不全是中文
+            }
+        }else{
+            if(preg_match('/^[\x7f-\xff]+$/', $str)){
+                return true;//全是中文
+            }else{
+                return false;//不全是中文
+            }
+        }
+    }
+?>
